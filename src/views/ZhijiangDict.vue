6bc6ac58d13d6e9e41ea29843796f5fb39c35159
AdamXuD <737173634xdh@gmail.com>
2021-10-26 11:15:17 +0800

    update: 为方言词典添加api

--- src/views/ZhijiangDict.vue
+++ src/views/ZhijiangDict.vue
@@ -16,12 +16,14 @@
             type="text"
             v-model="search.keyword"
             placeholder="在此处输入你想了解的词条"
+            @keyup.enter="onSearchBtnClicked"
           />
           <input
             type="submit"
             value="查询词条"
             :class="search.isBtnDisabled ? 'submit-disable' : 'submit-enable'"
             :disabled="search.isBtnDisabled"
+            @click="onSearchBtnClicked"
           />
         </div>
         <div class="entries-box">
@@ -55,7 +57,7 @@
                 <p class="dict-detail">{{ toPlainText(item.content) }}</p>
                 <p class="dict-updatetime">
                   <span class="ico-clock"></span
-                  >{{ ` 更新日期  ${timestampToDate(item.update_timestamp)}` }}
+                  >{{ ` 更新日期  ${timestampToDate(item.updated)}` }}
                 </p>
               </li>
             </ul>
@@ -67,9 +69,7 @@
           <span class="path">{{ detail.path.join("/") }}</span>
           <span class="update-time"
             ><span class="ico-clock"></span>
-            {{
-              ` 最近更新  ${timestampToDate(detail.entry.update_timestamp)}`
-            }}</span
+            {{ ` 最近更新  ${timestampToDate(detail.entry.updated)}` }}</span
           >
         </div>
         <h4 class="title">{{ detail.entry.title }}</h4>
@@ -82,7 +82,8 @@
 <script lang="ts">
 import "../assets/css/yue.css";
 import showdown from "showdown";
-import { defineComponent, ref, reactive } from "vue";
+import { defineComponent, reactive, ref } from "vue";
+import useCurrentInstance from "@/hooks/useCurrentInstance";
 
 interface CategoryNode {
   cid: number;
@@ -94,7 +95,7 @@ interface Entry {
   aid: number;
   title: string;
   content: string;
-  update_timestamp: number;
+  updated: number;
 }
 
 export default defineComponent({
@@ -129,7 +130,7 @@ export default defineComponent({
         aid: 0,
         title: "",
         content: "",
-        update_timestamp: 0,
+        updated: 0,
       },
       isShow: false,
       path: [],
@@ -138,8 +139,11 @@ export default defineComponent({
     let categories = Array<CategoryNode>();
     let entries = ref(Array<Entry>());
     let loading = ref(true);
+    const { proxy } = useCurrentInstance();
 
     return {
+      proxy,
+
       search, //搜索框相关组件数据
       tab, // tab相关组件数据
       detail, // 详情页相关组件数据
@@ -168,110 +172,39 @@ export default defineComponent({
         .replace(/>/g, " ");
     },
     // 获取所有分类节点列表
-    getCategories() {
-      // 后端返回数据样例
-      this.categories = [
-        {
-          cid: 7,
-          name: "V圈基本用语",
-          parent_cid: null,
-        },
-        {
-          cid: 8,
-          name: "主播方面",
-          parent_cid: 7,
-        },
-        {
-          cid: 9,
-          name: "观众方面",
-          parent_cid: 7,
-        },
-        {
-          cid: 10,
-          name: "字幕组方面",
-          parent_cid: 7,
-        },
-        {
-          cid: 11,
-          name: "平台与企业方面",
-          parent_cid: 7,
-        },
-        {
-          cid: 12,
-          name: "词句与其他",
-          parent_cid: 7,
-        },
-        {
-          cid: 13,
-          name: "ASOUL相关",
-          parent_cid: null,
-        },
-        {
-          cid: 26,
-          name: "ASOUL外V圈",
-          parent_cid: null,
-        },
-        {
-          cid: 52,
-          name: "其他网络文化",
-          parent_cid: null,
-        },
-        {
-          cid: 62,
-          name: "补充性词条",
-          parent_cid: null,
-        },
-      ];
+    async getCategories() {
+      this.categories = await this.proxy.$request({
+        url: "http://dict.asoulfan.com/api/v1/public/categories",
+      });
 
       this.onTabChanged(-1, null);
       this.loading = false;
     },
     // 获取当前分类下所有的词条
-    getEntries(cid: number) {
-      cid;
+    async getEntries(cid: number) {
       // 后端返回数据样例
-      const content = [
-        {
-          aid: 210,
-          title: "偷",
-          content:
-            "## 释义\n指复制粘贴、抄袭改编或借用创意等行为。\n在A-SOUL讨论语境中，“偷”多指那些粘贴经典定型文、重复整活玩梗等行为，主要是一种调侃，并非真正的指责。\n\n如：\n> 一眼偷;首偷，再偷必究；偷了；这段话又是哪偷的？\n\n## 注意\n**如果涉及实际的抄袭、剽窃，则不能用“偷”调侃过去，需要严肃对待。**\n\n\n",
-          update_timestamp: 1631081792,
+      let content = await this.proxy.$request({
+        url: "https://dict.asoulfan.com/api/v1/public/entries",
+        params: {
+          cid: cid,
         },
-        {
-          aid: 211,
-          title: "版本",
-          content:
-            "## 释义\n指当下粉丝社群中讨论的主要话题。\n由于v吧A吧等论坛的话题和风向变化较快，故用“版本更新”来形容话题更换的现象。\n\n如使用例：\n> 今天是什么版本？\n> 睡了一觉，版本又变了？",
-          update_timestamp: 1631081792,
-        },
-        {
-          aid: 212,
-          title: "发病",
-          content:
-            "## 释义\n较为强烈和魔怔地表达自己的感情。\n该词不具有贬义和讽刺义，仅仅是善意的调侃，“发病”也是互联网表达的乐趣和特色之一。",
-          update_timestamp: 1631081792,
-        },
-        {
-          aid: 213,
-          title: "紫砂",
-          content:
-            "## 释义\n自杀的谐音说法。\n“自杀”的开玩笑说法，常用于夸张地表示自己的情绪崩溃。\n\n如：\n> 珈乐还不回来，我紫砂了！\n> 由于嘉然请假到下周，本人宣布紫砂，并于本周内暂停生命活动。\n\n## 近同义\n/REMAKE；REMAKE；重开；\n\n## 注意\n**如果遇到心理健康问题，请及时就医咨询。珍惜生命，热爱生活。**\n",
-          update_timestamp: 1631081792,
-        },
-        {
-          aid: 214,
-          title: "小作文",
-          content:
-            "## 释义\n粉丝为表达真情实感而创作的文章。\n“小作文”指代的范围以积极正面为主，一般指玩梗搞笑或真心表达爱意的文章。\n而严肃批评、提出意见的文章一般不会被称为“小作文”。\n",
-          update_timestamp: 1631081792,
-        },
-      ];
+      });
       let converter = new showdown.Converter();
-      content.forEach((value) => {
+      content.forEach((value: Entry) => {
         value.content = converter.makeHtml(value.content);
       });
       this.entries = content;
+    },
+    async onSearchBtnClicked() {
+      if (!this.search.isBtnDisabled) {
+        this.entries = await this.proxy.$request({
+          url: "http://127.0.0.1:3000/api/v1/public/search",
+          params: {
+            kwd: this.search.keyword,
+          },
+        });
+        this.tab.active = [];
+      }
     },
     //当tab发生改变时
     onTabChanged(level: number, cid: number | null) {
