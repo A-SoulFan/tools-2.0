7f962b5cd01e5060aa42d0c1da432fe2d8cdaf5c
luooooob <contact@jiang-yan.com>
2021-11-13 22:32:46 +0800

    Fix v-html

--- src/views/DuplicateChecking.vue
+++ src/views/DuplicateChecking.vue
@@ -1,9 +1,7 @@
 <template>
   <headerTitle title="知网查重" sub-title="帮助你快速识别原创小作文" @buttonClick="changeIntroduceShow()"></headerTitle>
   <div v-show="isShowIntroduce" class="introduce-phone">
-    <div class="introduce-title">
-      功能介绍
-    </div>
+    <div class="introduce-title">功能介绍</div>
     <div v-for="(item, index) in contentList" :key="index" class="introduce-text-content">
       <div>{{ item.title }}</div>
       <div>{{ item.content }}</div>
@@ -34,9 +32,7 @@
         ></textarea>
         <div
           class="search-text-count"
-        >
-          总字数:{{ searchData.searchValue.length }}/{{ searchData.maxLength }}
-        </div>
+        >总字数:{{ searchData.searchValue.length }}/{{ searchData.maxLength }}</div>
         <div
           class="search-button"
           :style="{
@@ -44,9 +40,7 @@
               searchData.searchValue.length > 0 ? '#4B5563' : '#9CA3AF',
           }"
           @click="getDuplicate()"
-        >
-          查询成分
-        </div>
+        >查询成分</div>
       </div>
 
       <div class="result-area">
@@ -60,9 +54,7 @@
                 "
               >
                 <img src="@/assets/icons/BilibiliIcon.svg" />
-                <div class="result-item-author">
-                  {{ item.author }}
-                </div>
+                <div class="result-item-author">{{ item.author }}</div>
               </div>
               <div>查重率 {{ item.rate }}%</div>
             </div>
@@ -71,9 +63,7 @@
               复制
             </div>
           </div>
-          <div class="result-item-content">
-            {{ item.content }}
-          </div>
+          <div class="result-item-content">{{ item.content }}</div>
           <div class="result-item-flex">
             <div class="display-center">
               <img src="@/assets/icons/clock.svg" />
@@ -89,9 +79,7 @@
     </div>
     <div>
       <div class="introduce-pc">
-        <div class="introduce-title">
-          功能介绍
-        </div>
+        <div class="introduce-title">功能介绍</div>
         <div v-for="(item, index) in contentList" :key="index" class="introduce-text-content">
           <div>{{ item.title }}</div>
           <div>{{ item.content }}</div>
@@ -158,7 +146,7 @@ export default defineComponent({
     const isShowIntroduce = ref(false)
     const { proxy } = useCurrentInstance()
     // 方法
-    const getDuplicate = async() => {
+    const getDuplicate = async () => {
       const res = await proxy.$request({
         method: 'post',
         url: import.meta.env.VITE_API_DUPLICATECHECKING,
