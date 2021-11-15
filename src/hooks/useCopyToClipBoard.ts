ddd13e84a5d0d0355b003264b75c719a6737be1a
啊啊啊锋子阿 <linker_fen@163.com>
2021-11-15 21:55:23 +0800

    update: 成功复制到剪切板时提示

--- src/hooks/useCopyToClipBoard.ts
+++ src/hooks/useCopyToClipBoard.ts
@@ -1,3 +1,9 @@
+
+import { createApp } from 'vue'
+import App from '../App.vue'
+import Toast from '@/components/Toast/Toast'
+const app = createApp(App)
+app.use(Toast)
 // 辅助到剪切板
 const copyToClipBoard = (text: string): void => {
   const copyElement = document.body.appendChild(
@@ -10,6 +16,7 @@ const copyToClipBoard = (text: string): void => {
   copyElement.select()
   document.execCommand('Copy')
   copyElement.parentElement?.removeChild(copyElement)
+  app.config.globalProperties.$Toast.show('已复制到剪切板')
 }
 
 export default copyToClipBoard
