9f2e4be89a3214e451670d741cb08bd2aa0c50dd
锋子 <linker_fen@163.com>
2021-12-02 17:33:37 +0800

    update: 初步更新表情包样式逻辑

--- src/views/EmojiCollection.vue
+++ src/views/EmojiCollection.vue
@@ -1,247 +1,222 @@
 <!-- 今天溜什么-->
 <template>
-  <div>
-    <header-title title="表情包" sub-title="你想要的表情包都在这里"></header-title>
-    <div class="block"></div>
-    <div class="update-time-area">
-      <div class="time-left">
-        <img class="icon-clock" src="../assets/icons/clock.svg" />
-        <div class="update-time-text">
-          {{ "最近更新" + updateTime }}
+  <header-title title="表情包" sub-title="你想要的表情包都在这里"></header-title>
+  <div class="EmojiCollection">
+    <div class="waterfall" ref="waterfallBox">
+      <div
+        class
+        v-for="img in data.imgShowList"
+        :key="img.id"
+        :style="'width:' + img.width + 'px;height:' + img.height + 'px;'"
+      >
+        <img :src="'//' + img.url + `@` + img.width + 'w_' + img.height + 'h.webp'" />
+      </div>
+    </div>
+
+    <div>
+      <div class="introduce-pc">
+        <div class="introduce-title">功能介绍</div>
+        <div class="introduce-text-content">
+          <div class="introduce-text-content">请速度去b站给五小只点点关注捏(♡ ὅ ◡ ὅ )ʃ♡</div>
+          <div class="introduce-Asoul">
+            <div
+              v-for="item in Asoul"
+              :key="item.BzhanUid"
+              :style="'color:' + item.color"
+              class="introduce-Asoul-item"
+              @click="toBilibiliSpace(item.BzhanUid)"
+            >
+              <img :src="item.face" class="introduce-Asoul-face" />
+              <div class="introduce-Asoul-name">{{ item.name }}</div>
+            </div>
+          </div>
         </div>
       </div>
-
-      <button class="head-button">
-        刷新
-      </button>
-    </div>
-  </div>
-
-  <div class="button-area">
-    <button class="button">
-      A
-    </button>
-
-    <button class="button">
-      B
-    </button>
-
-    <button class="button">
-      C
-    </button>
-
-    <button class="button">
-      D
-    </button>
-
-    <button class="button">
-      E
-    </button>
-  </div>
-  <div class="block"></div>
-  <div id="app-mains" class="masonry">
-    <div v-for="item in res" :key="item.id" class="item">
-      <img :src="'https:' + item.url + '@518w.webp'" alt="加载错误" class="itemimage" />
-      <span class="item2">
-        <button class="itembutton" @click="getContent">
-          <img src="../assets/icons/coolicon.svg" class="cool" />
-        </button>
-      </span>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref, reactive } from 'vue'
 import headerTitle from '../components/HeaderTitle.vue'
 import useCurrentInstance from '@/hooks/useCurrentInstance'
 
 export interface itemObj {
-  id: string
+  id: number
   url: string
-  height: string
-  width: string
+  height: number
+  width: number
 }
 
 export default defineComponent({
   components: { headerTitle },
-
   setup() {
-    const Title = '表情包'
-    const subtitle = '你想要的表情包都在这里'
-    const updateTime = ref('2021.8.26 15:00')
-    const res = ref<itemObj[]>([])
-    const loading = ref(false)
+    const { proxy } = useCurrentInstance()
+    const waterfallBox = ref<null | HTMLElement>(null)
+    const Width = 250;
+    const boxWidth = ref<number>(0)
+    const Asoul = [
+      {
+        name: '向晚大魔王',
+        color: '#9ac8e2',
+        BzhanUid: 672346917,
+        face: 'https://i0.hdslb.com/bfs/face/566078c52b408571d8ae5e3bcdf57b2283024c27.jpg@256w_256h_1o.webp',
+      },
+      {
+        name: '贝拉Kira',
+        color: '#db7d74',
+        BzhanUid: 672353429,
+        face: 'https://i2.hdslb.com/bfs/face/668af440f8a8065743d3fa79cfa8f017905d0065.jpg@256w_256h_1o.webp',
+      },
+      {
+        name: '珈乐Carol',
+        color: '#b8a6d9',
+        BzhanUid: 351609538,
+        face: 'https://i2.hdslb.com/bfs/face/a7fea00016a8d3ffb015b6ed8647cc3ed89cbc63.jpg@256w_256h_1o.webp',
+      },
+      {
+        name: '嘉然今天吃什么',
+        color: '#e799b0',
+        BzhanUid: 672328094,
+        face: 'https://i2.hdslb.com/bfs/face/d399d6f5cf7943a996ae96999ba3e6ae2a2988de.jpg@256w_256h_1o.webp',
+      },
+      {
+        name: '乃琳Queen',
+        color: '#576690',
+        BzhanUid: 672342685,
+        face: 'https://i1.hdslb.com/bfs/face/8895c87082beba1355ea4bc7f91f2786ef49e354.jpg@256w_256h_1o.webp',
+      },
+    ]
+    const data = reactive({
+      imgShowList: [] as any[],
+      waterList: Array.from(Array(4), () => new Array(0)),
+      initDate: [] as itemObj[]
+    })
+    // const res = ref<itemObj[]>([])
     // 获取列表
 
-    const { proxy } = useCurrentInstance()
 
-    const getItem = async() => {
-      res.value = await proxy.$request({
+    const getEmojiList = async () => {
+      const res: Array<itemObj> = await proxy.$request({
         url: import.meta.env.VITE_API_EMOJI,
         params: {
           page: 1,
-          limit: 100,
+          limit: 20,
         },
       })
+      data.initDate.push(...res)
 
-      loading.value = true
-      loading.value = false
+      // data.imgShowList = res
+      let tempList = res.map((item: itemObj) => {
+        return {
+          width: Width,
+          height: Math.floor((item.height) / (item.width) * Width),
+          url: '//' + item.url,
+          id: item.id
+        }
+      }).sort((a, b) => {
+        return a.height - b.height
+      })
+      data.imgShowList = [...data.imgShowList, ...tempList]
+      console.log(data.imgShowList);
+
     }
 
-    getItem()
-
-    function getContent() {
-      window.open(`https${res.value}`)
+    const toBilibiliSpace = (uid: number) => {
+      window.open(`https://space.bilibili.com/${uid}`)
     }
+
     return {
-      Title,
-      subtitle,
-      updateTime,
-      col: 4,
-      loading,
-      res,
-      getContent,
+      data,
+      waterfallBox,
+      Asoul,
+      boxWidth,
+      Width,
+      toBilibiliSpace,
+      getEmojiList
     }
   },
+  async mounted() {
+    await this.getEmojiList()
+
+    window.onresize = () => {
+      this.boxWidth = this.waterfallBox!.clientWidth
+      console.log(this.boxWidth);
+      this.data.waterList = Array.from(Array(Math.floor(this.boxWidth / this.Width)), () => new Array(0))
+    }
+  }
 })
 </script>
 
 <style scoped lang="less">
-.update-time-area {
-  display: inline-flex;
-  margin-top: 23.5px;
-  margin-bottom: 25px;
-  /* height: 30px; */
-  align-items: center;
-}
-.time-left {
-  display: block;
-  float: left;
-  width: 100px;
-  height: 40px;
-  position: absolute;
-}
-
-.icon-clock {
-  width: 12px;
-  height: 12px;
-  margin-right: 2px;
-}
-.update-time-text {
-  display: inline;
-  font-size: 12px;
-  color: #4b5563;
-}
-
-.head-button {
-  position: absolute;
-  right: 100px;
-  height: 60px;
-  /* 0 */
-  background: #fff;
-  border: none;
-  font-size: 15px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 53px;
-  letter-spacing: 0em;
-  text-align: center;
-}
-.block {
-  width: 100%;
-  height: 20px;
-}
-.app-mains {
-  position: relative;
-  left: 100px;
-  top: 100px;
-}
-.button-area {
+.EmojiCollection {
   display: flex;
-  justify-content: space-around;
-}
-.button {
-  position: float;
-  width: 60px;
-  height: 60px;
-  left: 63px;
-  top: 573px;
-  /* 0 */
-  background: #f8f8f8;
-  border: none;
-  font-size: 20px;
-}
-.container {
-  display: inline;
-}
-.masonry {
-  width: auto; // 默认宽度
-  margin: 20px auto; // 剧中
-  columns: 4; // 默认列数
-  column-gap: 30px; // 列间距
+  margin-top: 20px;
+  color: #374151;
 }
 
-.item {
-  width: 100%;
-  break-inside: avoid;
-  margin-bottom: 30px;
-  -webkit-column-break-inside: avoid;
-  break-inside: avoid; /*防止断点*/
-  text-align: right;
-  position: relative;
+.waterfall {
+  border: 1px solid #374151;
+  flex: 1;
+  display: flex;
 }
 
-.item2 {
-  position: absolute;
-  right: 0;
-  bottom: 0;
+//
+.introduce-pc {
+  background-color: #f3f4f6;
+  width: calc(22.4vw - 40px);
+  min-width: 300px;
+  min-height: 200px;
+  margin-left: 20px;
+  padding: 20px;
+  border-radius: 2px;
 }
-.itemimage {
-  z-index: 2;
-  width: 100%;
+.introduce-title {
+  font-weight: 400;
+  font-size: 24px;
+  margin-bottom: 20px;
 }
-
-.item h2 {
-  padding: 8px 0;
-}
-.itembutton {
-  width: 35px;
-  height: 35px;
-  margin: -700px 0px 0 0;
-  background: #f8f8f8;
-  border: none;
-}
-.item p {
-  color: #555;
-}
-
-.cool {
-  height: 75%;
-  text-align: center;
-}
-@media screen and (min-width: 1024px) and (max-width: 1439.98px) {
-  .masonry {
-    width: 100%;
-    columns: 4;
-    column-gap: 20px;
+.introduce-text-content {
+  font-size: 15px;
+  .introduce-text-content-section {
+    margin-bottom: 20px;
   }
 }
-//ipad:
-
-@media screen and (min-width: 768px) and (max-width: 1023.98px) {
-  .masonry {
-    width: 100%;
-    columns: 3;
-    column-gap: 20px;
+.introduce-text-content-name {
+  margin: 0 10px;
+  cursor: pointer;
+  color: #666;
+  text-decoration: underline;
+}
+.introduce-Asoul {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  .introduce-Asoul-item {
+    margin: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    word-break: keep-all;
+    cursor: pointer;
+    width: 65px;
+  }
+  .introduce-Asoul-face {
+    width: 67px;
+    height: 67px;
+    border-radius: 50%;
+  }
+  .introduce-Asoul-name {
+    margin-top: 5px;
+    font-size: 12px;
   }
 }
-//mobile:
-
-@media screen and (max-width: 767.98px) {
-  .masonry {
-    width: 100%;
-    columns: 2;
+//
+@media only screen and (max-width: 768px) {
+  .introduce-pc {
+    display: none;
   }
 }
 </style>
+
