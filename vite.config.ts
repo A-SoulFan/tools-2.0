66261ad38264817c686a8493e093f4051597b12e
rosmontis233 <459595651@qq.com>
2022-03-27 00:00:55 +0800

    update:删除el自动导入,添加el tree-shaking插件;

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,11 @@
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import {ElementPlusResolver} from "unplugin-vue-components/resolvers";
+import ElementPlus from 'unplugin-element-plus/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    AutoImport({
-      resolvers:[ElementPlusResolver()]
-    }),
-    Components({
-      resolvers:[ElementPlusResolver()]
-    })
+    ElementPlus()
   ],
   resolve: {
     alias: [{ find: "@", replacement: "/src" }]
